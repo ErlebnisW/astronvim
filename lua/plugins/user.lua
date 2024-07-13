5c9f0c87e7296240b31601c22148a7e9114ea36d
ErlebnisW <787368748w@gmail.com>
2024-07-14 01:50:32 +0800

    add smart open & transparent plugin

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -8,6 +8,23 @@ return {
 
   -- == Examples of Adding Plugins ==
   {
+    "danielfalk/smart-open.nvim",
+    branch = "0.2.x",
+    config = function()
+      require("telescope").load_extension("smart_open")
+    end,
+    dependencies = {
+      "kkharji/sqlite.lua",
+      -- Only required if using match_algorithm fzf
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+      -- Optional.  If installed, native fzy will be used when match_algorithm is fzy
+      { "nvim-telescope/telescope-fzy-native.nvim" },
+    },
+    vim.keymap.set("n", "<leader><leader>", function ()
+    require("telescope").extensions.smart_open.smart_open()
+    end, { noremap = true, silent = true })
+  },
+  {
     'mrjones2014/legendary.nvim',
     -- since legendary.nvim handles all your keymaps/commands,
     -- its recommended to load legendary.nvim before other plugins
